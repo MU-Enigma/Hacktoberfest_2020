3d2ab74fac0cbf8de7ba616d2a33f1c2d0690f60
Kunal <46809814+Kunal888@users.noreply.github.com>
2020-10-10 18:58:08 +0530

    Update problem_6.py

--- Problems/Problem_6/problem_6.py
+++ Problems/Problem_6/problem_6.py
@@ -3,10 +3,9 @@ current_year = 2020
 
 years = 0   # initialise number of years found as leap
 
-print("The next 20 leap years are: OB")
+print("The next 20 leap years are: ")
 while years < 20:
     current_year += 1
     if current_year % 4 == 0:   # if the year is leap
         print(current_year)
         years += 1
-
