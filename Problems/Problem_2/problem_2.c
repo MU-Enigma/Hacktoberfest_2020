47b0a33a2e2ad814b5213ab0ca4cdd5a4f87c854
vignesh-gunda <69295385+vignesh-gunda@users.noreply.github.com>
2020-10-14 14:37:22 +0530

    added the second file

---
+++ Problems/Problem_2/problem_2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    long long int n;
+    printf("N = ")
+    scanf("%lld",&n);
+    int temp=n,a=0;
+    while(n>0)
+    {
+        int k=n%10;
+        if (temp%k == 0)
+        a=1;
+        n=n/10;
+    }
+    if(a==1)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+}
