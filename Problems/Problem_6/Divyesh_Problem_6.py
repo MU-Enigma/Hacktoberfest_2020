348fcad8f3751608353672a6309907da82c80e1b
DivyeshPoddar <divyeshsambhavpoddar@gmail.com>
2020-10-28 22:40:16 +0530

    Solved Problem 6

---
+++ Problems/Problem_6/Divyesh_Problem_6.py
@@ -0,0 +1,16 @@
+year = int(input("Enter the current year"))
+c = 1
+while c <= 20:
+    if year % 4 == 0:
+       while c <= 20:
+           print(year)
+           year += 4
+           c += 1
+    else:
+        year += 1
+
+
+
+
+
+
