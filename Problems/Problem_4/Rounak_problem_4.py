c62e5fa94486e8b4da6fdd47cddaf9955dea2ff4
Rounak-Das-02 <dasrounak2002@gmail.com>
2020-10-28 20:16:53 +0530

    new change

---
+++ Problems/Problem_4/Rounak_problem_4.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[30]:
+
+
+n = int(input("Enter the value of n : "))
+for i in range(1,n+2, 1):
+    for j in range(1,i):
+        print(j," ", end = " ")
+    print()
+        
+
+
+# In[ ]:
+
+
+
+
