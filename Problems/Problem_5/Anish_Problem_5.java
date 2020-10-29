08ef8cd33009c5826442ebc5fd8ea392cb837665
Anish528 <73593613+Anish528@users.noreply.github.com>
2020-10-29 12:27:57 +0530

    Add files via upload

---
+++ Problems/Problem_5/Anish_Problem_5.java
@@ -0,0 +1,17 @@
+package hacktoberfest;
+
+public class Anish_Problem_5 {
+
+	public static void main(String[] args) {
+		
+		
+		for(int i =1; i<=12;i++) {
+			
+			 int tableof = i;
+			 for (int j = 1;j<=10;j++) {
+				 System.out.print(tableof * j + " ");
+			 }System.out.println();
+	          }
+	}
+
+}
