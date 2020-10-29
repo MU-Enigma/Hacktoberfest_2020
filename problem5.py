7a9ff470de522d3683806c5d731b7d400949600a
doozmoco <73590997+doozmoco@users.noreply.github.com>
2020-10-29 13:28:56 +0530

    Add files via upload

---
+++ problem5.py
@@ -0,0 +1,10 @@
+i=1
+while(i<=12):
+    j=1
+    print(f"{i} Table")
+    print('---------')
+    while(j<=10):
+        print(f"| {i} * {j} = {i*j} |")
+        j=j+1
+    print('---------')
+    i = i+1
