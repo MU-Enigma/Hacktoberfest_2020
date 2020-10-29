62cbbf320db1b72f28cf60d3f9c606e43ca42bcf
Anish528 <73593613+Anish528@users.noreply.github.com>
2020-10-29 12:16:53 +0530

    Add files via upload

---
+++ Problems/Problem_6/Anish_Problem_6.java.java
@@ -0,0 +1,14 @@
+package hacktoberfest;
+
+public class Anish_Problem_6 {
+
+	public static void main(String[] args) {
+
+		
+		  int year;
+
+	        for ( year = 2000; year <= 2080; year = year +4) {
+	            System.out.println(year+ " are the leap years ");}
+	}
+
+}
