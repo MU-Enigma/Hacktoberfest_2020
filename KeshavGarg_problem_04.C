db0441bf5e660eb8126f77beb2041105a3bc323c
gargkeshav2002 <73296223+gargkeshav2002@users.noreply.github.com>
2020-10-29 11:49:10 +0530

    KeshavGarg_problem_04.C
    Solution to problem 4

---
+++ KeshavGarg_problem_04.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+ int n,i,j;
+ clrscr();
+ printf("Enter the value of n:");
+ scanf("%d",&n);
+ for(i=1;i<=n;i++)
+ {
+  for(j=1;j<=i;j++)
+  {
+   printf("%d ",j);
+  }
+  printf("\n");
+ }
+ getch();
+}
