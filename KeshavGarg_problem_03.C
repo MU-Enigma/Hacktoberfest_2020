9348e0cee08cb09ad80e30921617a80150672f85
gargkeshav2002 <73296223+gargkeshav2002@users.noreply.github.com>
2020-10-29 05:53:26 +0530

    Add files via upload

---
+++ KeshavGarg_problem_03.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+ int a,b,c;
+ clrscr();
+ printf("Enter three numbers(a,b,c);");
+ scanf("%d %d %d",&a,&b,&c);
+ if(a*a==b*b+c*c)
+ printf("%d %d %d",a,b,c);
+ else if(b*b==a*a+c*c)
+ printf("%d %d %d",b,a,c);
+ else if(c*c==a*a+b*b)
+ printf("%d %d %d",c,a,b);
+ else
+ printf("NOPE!");
+ getch();
+}
