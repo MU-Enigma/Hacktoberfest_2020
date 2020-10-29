ee76487dd55f545317fca1cb43e4b0ffac5de3d6
Mihir Ranjan <mihirranjan@Mihirs-MacBook-Air.local>
2020-10-29 22:21:01 +0530

    MihirRanjan_Problem5

---
+++ Problems/Problem_5/MihirRanjan_Problem5.java
@@ -0,0 +1,15 @@
+package hacktocberfest;
+
+public class problemFive {
+
+	public static void main(String[] args) {
+		for(int i = 1; i<=12 ; i ++) {
+			for(int j=1; j<=10 ; j++) {
+				System.out.println(i*j + " ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
