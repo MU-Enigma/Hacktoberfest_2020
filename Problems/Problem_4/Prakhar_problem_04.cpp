f3a1f2f9689540001752ed075bfb27171e34f11a
prak12g <73539086+prak12g@users.noreply.github.com>
2020-10-29 22:42:08 +0530

    Add files via upload

---
+++ Problems/Problem_4/Prakhar_problem_04.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+int main() 
+
+{
+  
+int c =0;
+ 
+std::cout<<"enter the value of n";
+  
+std::cin>>c;
+  
+for ( int i = 1 ; i <= c ; i++)
+  
+{
+    for ( int j=1 ; j <= i ; j++)
+    
+ {
+     std::cout<< j ;
+    
+   
+ }
+     std::cout<<"\n";
+ 
+ }
+
+}
