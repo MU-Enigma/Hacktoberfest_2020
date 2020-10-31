9e56703b6000e3b93eab2fc9dd3b29d5fcc667c1
sailahar5 <42346690+sailahar5@users.noreply.github.com>
2020-10-31 17:57:21 +0530

    Add files via upload

---
+++ Problems/Problem_1/lahar_prob1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	inti,j,n;
+	printf("no of rows");
+	scanf("%d",&r);
+	for(i=1;i<=n;i++)
+{
+	for(j=1;j<=i;j++)
+	printf("%d", j);	
+	printf("/n");
+}
+return0;
+}
